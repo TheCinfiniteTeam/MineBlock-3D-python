fedf0b7adb34163400c5acb15ccfa9e4ae716147
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-09-08 21:35:39 +0800

    Update mineblock.py

--- com/github/thecinfiniteteam/mineblock/mineblock/mineblock.py
+++ com/github/thecinfiniteteam/mineblock/mineblock/mineblock.py
@@ -1,2 +1,5 @@
 #encoding:utf-8
 import com.github.thecinfiniteteam.mineblock.mineblock.message.api_io as msg_io
+MINEBLOCK_ID = "mineblock"
+MINEBLOCK_NAME = "MineBlock"
+MINEBLOCK_VERSION = "1.0.0"
